a97198d4be825d409ffb7b69b1de71307dae333e
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-07-29 15:25:23 +0100

    refactoring code a little bit

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const record_el = document.getElementById('record');
 const blank_black_board_el = document.getElementById('blank_black_board');
 const record_wrapper_el= document.getElementById('record_wrapper');
 const vid_wrapper_el= document.getElementById('vid_wrapper');
-const vid_el= document.getElementById('vid');
+// const vid_el= document.getElementById('vid');
 
 
-console.log( vid_wrapper_el.srcObject);
+
 const gdmOptions = {
     video: {
       cursor: "always"
@@ -19,25 +19,42 @@ const gdmOptions = {
   }
 
 
+  function appendStreamToVideo(capturedVideoStream){
+    vid_wrapper_el.srcObject =new MediaStream(capturedVideoStream);
+  }
+  
+  async function newRecordWithStream(capturedVideoStream){
+    // const mediaStreamTrack =new MediaStream(capturedVideoStream);
+    const recordedDetails =[];
+    const record = new MediaRecorder(capturedVideoStream,{ mimeType: "video/webm" });
+    record.ondataavailable =(e)=>{
+      recordedDetails.push(e.data);
+    }
+    console.log(recordedDetails);
+  }
+  
+  async function screenCapture(){
+    let capturedVideoStream ='';
+      try{
+          vid_wrapper_el.style.display='block';
+          capturedVideoStream= await navigator.mediaDevices.getDisplayMedia(gdmOptions);
+      }
+      catch(e){
+          console.log(e.message);
+      }
+      let recordedMedia = new MediaRecorder(capturedVideoStream,{ mimeType: "video/webm" });
+      recordedMedia.start();
+
+       appendStreamToVideo(capturedVideoStream);
+      // record the mediaStream
+       newRecordWithStream(capturedVideoStream);
+      
+  }
+
 record_el.onclick = ()=>{
     blank_black_board_el.remove();
     record_wrapper_el.remove();
-    userMedia();
+     screenCapture();
     
 }
 
-async function userMedia(){
-    
-    try{
-        vid_wrapper_el.style.display='block';
-       const check=  await navigator.mediaDevices.getDisplayMedia(gdmOptions);
-        vid_wrapper_el.srcObject = await navigator.mediaDevices.getDisplayMedia(gdmOptions);
-        // console.log(vid_wrapper_el.srcObject);
-    
-    }
-    catch(e){
-        console.log(e.message);
-    }
-   
-    
-}
