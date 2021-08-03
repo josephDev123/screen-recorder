70ed180a10a3842bb5db33304cfcdf21286910cb
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-08-03 17:08:55 +0100

    mediaRecorder continues debugging

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 const record_el = document.getElementById('record');
 const blank_black_board_el = document.getElementById('blank_black_board');
 const record_wrapper_el= document.getElementById('record_wrapper');
-const vid_wrapper_el= document.getElementById('vid_wrapper');
-// const vid_el= document.getElementById('vid');
+const screenRecording_vid_el = document.getElementById('screenRecording_vid');
+const recorded_vid_wrapperel_el =document.getElementById('recorded_vid_wrapper');
+const message_el = document.getElementById('message');
+const play_el = document.querySelector('.play');
+const pause_el = document.querySelector('.pause');
+const download_el = document.querySelector('.download');
+play_el.disabled =true;
+pause_el.disabled =true;
+download_el.disabled =true;
+
+let mediaRecorder
+ 
 
 
 
@@ -18,43 +28,47 @@ const gdmOptions = {
     }
   }
 
+   function stopState(stream){
+      let checkStop =  new MediaRecorder(stream, {mimeType:"video/webm"});
+      checkStop.ondataavailable =(e)=>{
+        if(e.data && e.data.size > 0){
+          checkStop.stop();
+          play_el.disabled =false;
+          pause_el.disabled =false;
+          download_el.disabled =false; 
+          console.log('hello');
+        }
+      }
+    //  console.log(checkStop);
+  }
 
-  function appendStreamToVideo(capturedVideoStream){
-    vid_wrapper_el.srcObject =new MediaStream(capturedVideoStream);
+  function displayCaptureStreamToCaptureVideoEl(mediaRecorder){
+    screenRecording_vid_el.style.display ='block';
+    screenRecording_vid_el.srcObject = mediaRecorder;
+    
   }
-  
-  async function newRecordWithStream(capturedVideoStream){
-    // const mediaStreamTrack =new MediaStream(capturedVideoStream);
-    const recordedDetails =[];
-    const record = new MediaRecorder(capturedVideoStream,{ mimeType: "video/webm" });
-    record.ondataavailable =(e)=>{
-      recordedDetails.push(e.data);
-    }
-    console.log(recordedDetails);
-  }
+
   
   async function screenCapture(){
-    let capturedVideoStream ='';
-      try{
-          vid_wrapper_el.style.display='block';
-          capturedVideoStream= await navigator.mediaDevices.getDisplayMedia(gdmOptions);
-      }
-      catch(e){
-          console.log(e.message);
-      }
-      let recordedMedia = new MediaRecorder(capturedVideoStream,{ mimeType: "video/webm" });
-      recordedMedia.start();
-
-       appendStreamToVideo(capturedVideoStream);
-      // record the mediaStream
-       newRecordWithStream(capturedVideoStream);
-      
+    
+    try {
+      mediaRecorder = await navigator.mediaDevices.getDisplayMedia(gdmOptions);
+      stopState(mediaRecorder);
+      displayCaptureStreamToCaptureVideoEl(mediaRecorder);
+     
+    } catch (error) {
+      message_el.textContent = error.message;
+    }
+    
+    
   }
 
 record_el.onclick = ()=>{
     blank_black_board_el.remove();
     record_wrapper_el.remove();
+    // play_el.disabled =true
      screenCapture();
     
 }
 
+// console.log(mediaRecorder);
