2b14030241cc2197dbe93da746e0bd7ee888d0d8
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-08-12 18:24:46 +0100

    modified:   index.html         modified:   script.js         modified:   style.css

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ play_el.disabled =true;
 pause_el.disabled =true;
 download_el.disabled =true;
 
-let mediaRecorder
+let stream;
+let mediaRecorder;
+
  
 
 
@@ -28,19 +30,6 @@ const gdmOptions = {
     }
   }
 
-   function stopState(stream){
-      let checkStop =  new MediaRecorder(stream, {mimeType:"video/webm"});
-      checkStop.ondataavailable =(e)=>{
-        if(e.data && e.data.size > 0){
-          checkStop.stop();
-          play_el.disabled =false;
-          pause_el.disabled =false;
-          download_el.disabled =false; 
-          console.log('hello');
-        }
-      }
-    //  console.log(checkStop);
-  }
 
   function displayCaptureStreamToCaptureVideoEl(mediaRecorder){
     screenRecording_vid_el.style.display ='block';
@@ -48,20 +37,42 @@ const gdmOptions = {
     
   }
 
+  async function changeCapturedStreamToRecordedMedia(stream){
+    let recordedObj =[];
+      mediaRecorder = await new MediaRecorder(stream);
+      mediaRecorder.start();
+      mediaRecorder.ondataavailable = (e)=>{
+        if (e.data) {
+          recordedObj.push(e.data);
+        }
+      }
+
+      mediaRecorder.onstop =()=>{
+        const blob = new Blob(recordedObj, {type:recordedObj[0].type})
+        console.log(recordedObj);
+        recorded_vid_wrapperel_el.src = URL.createObjectURL(blob);
+        screenRecording_vid_el.style.display ='none';
+        recorded_vid_wrapperel_el.style.display ='block';
+        recorded_vid_wrapperel_el.style.display ='block';
+        document.querySelector('.play_pause_downlaod_wrapper').style.display ='block';
+      }
+
+  }
+  
+
   
   async function screenCapture(){
     
     try {
-      mediaRecorder = await navigator.mediaDevices.getDisplayMedia(gdmOptions);
-      stopState(mediaRecorder);
-      displayCaptureStreamToCaptureVideoEl(mediaRecorder);
+      stream = await navigator.mediaDevices.getDisplayMedia(gdmOptions);
+      //  stopState(mediaRecorder);
+      displayCaptureStreamToCaptureVideoEl(stream);
+      changeCapturedStreamToRecordedMedia(stream);
      
-    } catch (error) {
-      message_el.textContent = error.message;
-    }
-    
-    
+  }catch (error) {
+    message_el.textContent = error.message;
   }
+}
 
 record_el.onclick = ()=>{
     blank_black_board_el.remove();
@@ -71,4 +82,3 @@ record_el.onclick = ()=>{
     
 }
 
-// console.log(mediaRecorder);
